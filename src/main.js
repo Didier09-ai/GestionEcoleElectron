296588e2b30d8d4e9a627f89404d47c880ad3fa9
Didier <didiergbedan12@gmail.com>
2024-11-18 12:51:19 +0100

    Branche principale a ne pas toucher

--- src/main.js
+++ src/main.js
@@ -11,4 +11,16 @@ function createWindow() {
   win.loadFile('src/index.html');
 }
 
-app.on('ready', createWindow);
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
